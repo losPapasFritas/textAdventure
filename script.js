3bb047de5034295a919dfea12b2e87922e92a1ad
losPapasFritas <143848605+losPapasFritas@users.noreply.github.com>
2024-04-03 13:02:48 -0700

    added code to index,script,and style
    fixed desc issues, added more to script, added invis stuff

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let fullMain = `<section>Choose a character! <br><br> <button id="paul">Paul the Knight</button><button id="hannah">Hannah the Magician</button><button id="mathew">Mathew the Tamer</button></section>`
-    document.getElementById(`paul`).addEventListener(`click`,function(){
-        fullMain+=`Paul is a knights apprentice, who has wored hard to get to where he is. He has `;
-    });
+    let paulAll = document.getElementsByClassName(`paul`);
+    let hannahAll = document.getElementsByClassName(`hannah`);
+    let mathewAll = document.getElementsByClassName(`mathew`);
+    for(item of paulAll){
+        item.addEventListener(`click`,function(){
+            document.getElementById(`paulDesc`).classList.remove(`hidden`);
+            document.getElementById(`paulDesc`).classList.add(`visible`);
+            document.getElementById(`hannahDesc`).classList.remove(`visible`);
+            document.getElementById(`hannahDesc`).classList.add(`hidden`);
+            document.getElementById(`mathewDesc`).classList.remove(`visible`);
+            document.getElementById(`mathewDesc`).classList.add(`hidden`);
+        });
+    }
+    for(item of mathewAll){
+        item.addEventListener(`click`,function(){
+            document.getElementById(`mathewDesc`).classList.remove(`hidden`);
+            document.getElementById(`mathewDesc`).classList.add(`visible`);
+            document.getElementById(`hannahDesc`).classList.remove(`visible`);
+            document.getElementById(`hannahDesc`).classList.add(`hidden`);
+            document.getElementById(`paulDesc`).classList.remove(`visible`);
+            document.getElementById(`paulDesc`).classList.add(`hidden`);
+        });
+    }
+    for(item of hannahAll){
+        item.addEventListener(`click`,function(){
+            document.getElementById(`hannahDesc`).classList.remove(`hidden`);
+            document.getElementById(`hannahDesc`).classList.add(`visible`);
+            document.getElementById(`paulDesc`).classList.remove(`visible`);
+            document.getElementById(`paulDesc`).classList.add(`hidden`);
+            document.getElementById(`mathewDesc`).classList.remove(`visible`);
+            document.getElementById(`mathewDesc`).classList.add(`hidden`);
+        });
+    }
 });
