6105dbe95bec3d899590148cd2417bf320848bc0
losPapasFritas <143848605+losPapasFritas@users.noreply.github.com>
2024-04-11 15:49:02 -0700

    fixed small item bug

--- script.js
+++ script.js
@@ -245,7 +245,7 @@ function combatSetup(setCombat = 0, eIndx = 1) {
 
 function items(){
     allMain+=`<br><br><br><br><section>`
-    if(pItems.length > 1){
+    if(pItems.length > 0){
     for(item of pItems){
         allMain+=`<button onclick='useItems(${pItems.indexOf[item]})'>${item.itemName}</button>`
     }
